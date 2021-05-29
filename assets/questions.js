070fae9afac22a0d6907b89c7b58d28425b97aeb
BA1bers <brent.albers@yahoo.com>
2021-05-29 13:42:32 -0700

    added new questions and started in script

--- assets/questions.js
+++ assets/questions.js
@@ -3,7 +3,7 @@ let questions = [
         numb: 1,
         question: "What does HTML stand for?",
         answer: "Hyper Text Markup Language",
-        options: [
+        choices: [
             "Hyper Text Preprocessor",
             "Hyper Text Markup Language",
             "Hyper Text Multiple Language",
@@ -14,7 +14,7 @@ let questions = [
         numb: 2,
         question: "What does CSS stand for?",
         answer: "Cascading Style Sheet",
-        options: [
+        choices: [
             "Common Style Sheet",
             "Colorful Style Sheet",
             "Computer Style Sheet",
@@ -23,35 +23,46 @@ let questions = [
     },
     {
         numb: 3,
-        question: "What does PHP stand for?",
-        answer: "Hypertext Preprocessor",
-        options: [
-            "Hypertext Preprocessor",
-            "Hypertext Programming",
-            "Hypertext Preprogramming",
-            "Hometext Preprocessor"
+        question: "Commonly used data types DO NOT Include",
+        answer: "Alerts",
+        choices: [
+            "Booleans",
+            "Numbers",
+            "Strings",
+            "Alerts"
         ]
     },
     {
         numb: 4,
-        question: "What does SQL stand for?",
-        answer: "Structured Query Language",
-        options: [
-            "Stylish Question Language",
-            "Stylesheet Query Language",
-            "Statement Question Language",
-            "Structured Query Language"
+        question: "The condition in an if/else statement is ecnlosed with _______.",
+        answer: "Parenthesis",
+        choices: [
+            "Quotes",
+            "Curly Brackets",
+            "Square Brackets",
+            "Parenthesis"
         ]
     },
     {
         numb: 5,
-        question: "What does XML stand for?",
-        answer: "eXtensible Markup Language",
-        options: [
-            "eXtensible Markup Language",
-            "eXecutable Multiple Language",
-            "eXTra Multi-Program Language",
-            "eXamine Multiple Language"
+        question: "Arrays in Javascript can be used to store _______.",
+        answer: "All of the Above",
+        choices: [
+            "Other Arrays",
+            "Booleans",
+            "Numbers and Strings",
+            "All of the Above"
         ]
     },
+    {
+        numb: 6,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console.log",
+        choices: [
+            "Terminal/Bash",
+            "Console.log",
+            "Javascript",
+            "For Loops"
+        ]
+    }
 ];
